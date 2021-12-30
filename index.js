c6146977f62ca91b62e7636f386b35b86119b792
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-29 20:40:14 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+function reverseArray(word){
+  // const singleLetter = word.split("")
+  // const reverseLetters = singleLetter.reverse()
+  // const combineLetters = reverseLetters.join('')
+  return word.split("").reverse().join('')
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the word
+  const reverseWord = reverseArray(word)
+  return (word === reverseWord)
 }
 
 /* 
   Add your pseudocode here
+  reverse the word
+  check the word against the reversed word
+  return true if matching 
+  return false if not
 */
 
 /*
   Add written explanation of your solution here
+  Get the original word and reverse the word and then check it against the original word 
 */
 
 // You can run `node index.js` to view these console logs
